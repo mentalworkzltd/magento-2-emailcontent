eee837918ee4d3a2b5a1d11944c60a39823662c9
Shaun Clifford <shaun@develodesign.co.uk>
2024-07-10 10:30:38 +0100

    Ui component folder name bugfix

--- UI/Component/Listing/Column/Store.php
+++ UI/Component/Listing/Column/Store.php
@@ -7,6 +7,7 @@
  * @copyright   Copyright (c) Mentalworkz (https://www.mentalworkz.co.uk/)
  * @author      Shaun Clifford
  */
+
 declare(strict_types=1);
 
 namespace Mentalworkz\EmailContent\Ui\Component\Listing\Column;
